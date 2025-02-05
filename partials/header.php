2c5ecde95dc2df78f230c9388aa7b2b967608d0e
Davinaputku <davinaputri2408@gmail.com>
2025-02-05 21:35:42 +0700

    halaman post

--- partials/header.php
+++ partials/header.php
@@ -18,7 +18,7 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link active" href="<?= BASE_URL; ?>">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Post</a>
+                    <a class="nav-item nav-link" href="post.php">Post</a>
                     <a class="nav-item nav-link" href="#">About</a>
                 </div>
             </div>
---
+++ post.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+require_once 'functions.php';
+
+$posts = getPosts();
+
+if (isset($_GET['id'])) {
+    $id_postingan = $_GET['id']; 
+    echo "ID Postingan: " . $id_postingan;
+} else {
+    echo "ID tidak ditemukan di URL.";
+    exit;
+}
+
+?>
+
+<?php require_once 'partials/header.php' ?>
+
+
+
+<?php require_once 'partials/footer.php'?>
